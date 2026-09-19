c02d79c9e1ad29f4b7f9b44ac366abc605ee13aa
octavia <kiranabjb09@gmail.com>
2026-09-19 15:50:57 +0800

    Variabel.java

---
+++ Variaabel003.java
@@ -0,0 +1,22 @@
+public class Variaabel003 {
+    public static void main(String[] args) {
+     int nim1;
+     int nim2;
+     String nama1;
+     String nama2;
+     
+     //isi variabel
+     nim1 = 123;
+     nim2 = 234;
+     nama1 = "adi";
+     nama2 = "budi";
+
+     //cetak variabel
+     System.out.println("==============");
+     System.out.println("NIM   |   NAMA");
+     System.out.println("==============");
+     System.out.println(nim1 + "   |   " + nama1);
+     System.out.println(nim2 + "   |   " + nama2);
+     System.out.println("===============");
+    }
+}
